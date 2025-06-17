1fd90b37b593a19e57dab83023520e0c66958ace
Jang-HW <hw11515@naver.com>
2025-06-17 19:11:02 +0900

    row column / button

--- app/src/main/java/com/example/moveup_re/MainActivity.kt
+++ app/src/main/java/com/example/moveup_re/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.moveup_re.ui.theme.MoveUP_reTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,67 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MoveUP_reTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+){
+    Column(modifier) {
+        for(name in names){
+            Greeting(name)
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var expanded by remember { mutableStateOf(false) }
+    //  val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if(expanded) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = modifier.fillMaxWidth().padding(24.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ){
+                Text(text = "Hello")
+                Text(text = "$name!")
+            }
+            ElevatedButton(
+                onClick = { expanded = !expanded },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(if(expanded) "Show less" else "Show more")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MoveUP_reTheme {
-        Greeting("Android")
+        Greeting("Greeting")
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun MyAppPreview() {
+    MoveUP_reTheme {
+        MyApp()
     }
 }
