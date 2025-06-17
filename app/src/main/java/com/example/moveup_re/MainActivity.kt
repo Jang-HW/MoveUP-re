8f4419c0414d8746bfb58f291049d4740d22e92b
Jang-HW <hw11515@naver.com>
2025-06-17 23:10:58 +0900

    Hoisting / Keeping state / Animating / Theme

--- app/src/main/java/com/example/moveup_re/MainActivity.kt
+++ app/src/main/java/com/example/moveup_re/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,9 +24,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import com.example.moveup_re.ui.theme.MoveUP_reTheme
 
@@ -36,12 +46,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(
-    modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+fun MyApp(modifier: Modifier = Modifier){
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if(shouldShowOnboarding)
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        else
+            Greetings()
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
 ){
-    Column(modifier) {
-        for(name in names){
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement  = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ){
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }
+){
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name)
         }
     }
@@ -49,24 +91,32 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var expanded by remember { mutableStateOf(false) }
-    //  val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if(expanded) 48.dp else 0.dp
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    //  val expanded = rememberSaveable  { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(
-            modifier = modifier.fillMaxWidth().padding(24.dp)
-        ) {
+        Row(modifier = modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ){
                 Text(text = "Hello")
-                Text(text = "$name!")
+                Text(
+                    text = "$name!",
+                    style = MaterialTheme.typography.headlineMedium
+                )
             }
             ElevatedButton(
                 onClick = { expanded = !expanded },
@@ -78,11 +128,21 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+/*
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun GreetingPreview() {
+fun OnboardingPreview() {
     MoveUP_reTheme {
-        Greeting("Greeting")
+        OnboardingScreen(onContinueClicked = {}) // Do nothing on click.
+    }
+}
+*/
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingsPreview() {
+    MoveUP_reTheme {
+        Greetings()
     }
 }
 
@@ -90,6 +150,6 @@ fun GreetingPreview() {
 @Composable
 fun MyAppPreview() {
     MoveUP_reTheme {
-        MyApp()
+        MyApp(Modifier.fillMaxSize())
     }
 }
